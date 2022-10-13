477ab0e13b554164ba3a02a9166fe9c72c713cbb
Alex Nico <a.nicolaidis@outlook.com>
2022-10-13 11:52:09 +1100

    Added comments to code and fixed bugs

--- js/dom.js
+++ js/dom.js
@@ -1,53 +1,55 @@
-let $startButton;
-
 $(document).ready(function(){
 
-    
-    //depending on the turn number select the correct url for the image.
-    
-    const crossURL = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Red_X.svg/1200px-Red_X.svg.png"
-    const circleURL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW59vKiyqzPYfYMMIKP_Oe6PTcW3LCxhzFyA&usqp=CAU";
-
     $('div.square').on('click',function(){
-        gameInfo.addChoice($(this).attr('class').slice(-2)); 
+        //gets the position on the board that was selected and adds to the players choices
+        gameInfo.addChoice($(this).attr('class').slice(-2));
+        // checked class added css removes the functionality of the chosen squares
         $(this).addClass(`checked ${gameInfo.checkTurn()}`);
+        //
         $(this).parent().removeClass(gameInfo.checkTurn());
         gameInfo.turnCounter = Number(gameInfo.turnCounter) + 1;
         $(this).parent().addClass(gameInfo.checkTurn());
-        if(gameInfo.checkWin()){
-            for (i = 0; i < gameInfo.winSquares.length; i++){
-                let position = gameInfo.winSquares[i];
-                $(`div.square.position${position}`).addClass('animate');
-                $('.game-board').addClass('hold-for-start');
-            }
-            //update the score
-            gameInfo.updateScores()
-            $('.score1').html(gameInfo.score1);
-            $('.score2').html(gameInfo.score2);
 
-            //add the winner to the paragraph text
-            $('.win-message').html(gameInfo.winnerText());
+        //win can only occur after a minimum of 5 turns
+        if(gameInfo.turnCounter >=5){
+            //check for a win and animate the winning positions by adding a class to winning squares.
+            if(gameInfo.checkWin()){
+                for (i = 0; i < gameInfo.winSquares.length; i++){
+                    let position = gameInfo.winSquares[i];
+                    $(`div.square.position${position}`).addClass('animate');
+                    $('.game-board').addClass('hold-for-start');
+                }
+                //update the score and show win/tie message to the page
+                gameInfo.updateScores()
+                $('.score1').html(gameInfo.score1);
+                $('.score2').html(gameInfo.score2);
+                $('.win-message').html(gameInfo.winnerText());
+            }
         }
-        });
+    });
 
     $('.next-round').on('click',function () {
-        gameInfo.winner = '';
+        //refresh the gameboard and remove/add classes affected during the game
         $('.win-message').html('');
         $('.game-board').removeClass('hold-for-start');
         $('.square').removeClass('x o checked animate');
         $('.game-board').removeClass('x o');
         $('.game-board').addClass('x');
+        //clear information stored in the game object.
         gameInfo.nextRound();
     })
 
+    //store and add the input name of a player to the webpage or give a default name if name wasn't provided
     const join1 = function () {
         let name1 = gameInfo.player1;
         if($('.name1').val() !== ''){
-            name1 = $('.name1').val();
+            name1 = $('.name1').val()
             gameInfo.player1 = name1;
         }
         $('section.player1').html(`<p class = 'player1'>${name1} : Score <span class = "score1">0</span></p>`);
     }
+
+    //store and add the input name of a player to the webpage or give a default name if name wasn't provided
     const join2 = function () {
         let name2 = gameInfo.player2;
         if($('.name2').val() !== ''){
@@ -58,34 +60,47 @@ $(document).ready(function(){
     }
 
     const startButton = function (){
-        console.log('click')
+        //auto press join buttons to populate defult or player names if they were provided or not
         $('.join1').trigger('click')
         $('.join2').trigger('click')
+        // allow the game to be played
         $('.game-board').removeClass('hold-for-start');
+        // remove the start button and provide a container to pass a win/tie message
         $('.start').replaceWith("<p class = 'win-message'></p>")
+        //hides the message of "press start game to play"
         $('div.press-start').css('height','0');
         $('h3').html('Players');
     }
 
-    $('.start').on('click', startButton);
-    $('.join1').on('click',join1)
-    $('.join2').on('click',join2)
-
     $('.reset-full').on('click', function (){
+        //bring back the start button and remove win/tie message field
         $('.win-message').replaceWith('<button class = "start button">Start Game</button>')
         $('div.press-start').removeClass('.hide')
+        //bring back player name input fields and join buttons
         $('p.player1').replaceWith('<input class = "input name1" type="text" placeholder="Enter Player 1 Name"> <button class = "join1 button">Join</button>');
         $('p.player2').replaceWith('<input class = "input name2" type="text" placeholder="Enter Player 2 Name"> <button class = "join2 button">Join</button>');
+        const $join1 = $('.join1');
+        const $join2 = $('.join2');
+        //add back preventing game class. 
         $('.game-board').addClass('hold-for-start');
+        //give the new start button an event listener
         $('.start').on('click', startButton);
-        $('.join1').on('click',join1)
-        $('.join2').on('click',join2)
-        gameInfo.winner = '';
+        //ensures that any existing event handlers are removed from prior to re-adding
+        $join1.off('click');
+        $join2.off('click');
+        //need to add event handlers as in some cases the join buttons are removed to play the game and are re-aded on a re-set
+        $join1.on('click',join1)
+        $join2.on('click',join2)
+        //refresh the window and remove/add classes affected during the game
         $('.square').removeClass('x o checked animate');
         $('.game-board').removeClass('x o');
         $('.game-board').addClass('x');
-        gameInfo.resetGame();
         $('div.press-start').css('height','500px');
         $('h3').html('Players Enter Names');
+        gameInfo.resetGame();
     })
+    //add event listeners to the start and join buttons
+    $('.start').on('click', startButton);
+    $('.join1').on('click',join1)
+    $('.join2').on('click',join2)
 });
--- js/tic-tack-toe.js
+++ js/tic-tack-toe.js
@@ -1,19 +1,18 @@
-//build the logic for a tic-tac-toe game
-
-//possible game options
-// const gameChoices = [['00','01','02','10','11','12','20','21','22']];
-
+//All game information is sorted in the object
 const gameInfo = {
-    xChoices: [],
-    oChoices: [],
-    turnCounter: 0,
-    player1: 'Player 1',
+    xChoices: [], 
+    oChoices: [], 
+    turnCounter: 0, 
+    player1: 'Player 1', //default names if none is selected 
     player2: 'Player 2',
-    score1: 0,
+    score1: 0,           //score tally of both players
     score2: 0,
-    winOptions: [['00','01','02'],['10','11','12'], //setup the game board as an array of arrays
+    // board setup as 3x3 matix rows 0->2 and columns 0->2 possible win options stored in array of arrays
+    winOptions: [['00','01','02'],['10','11','12'], 
     ['20','21','22'],['00','10','20'],['01','11','21'],
     ['02','12','22'],['00','11','22'],['02','11','20']],
+    winSquares: [], //array to sort the positions of the winning pieces
+    winner: '', // variable to sort the winners message. 
     addChoice: function(choice){
         if (this.turnCounter % 2 === 0){
             this.xChoices.push(choice);
@@ -21,6 +20,7 @@ const gameInfo = {
             this.oChoices.push(choice);
         }
     },
+    //return a value to add as a class in the html file via jQuery 
     checkTurn:function (){
         if (this.turnCounter % 2 === 0){
             return 'x';
@@ -29,31 +29,38 @@ const gameInfo = {
         }
     },
     checkWin: function(){
-        let winner = '';
         for (i = 0; i < this.winOptions.length; i++){
+            //consider each possible win option
             let testWin = this.winOptions[i];
+            //check if the current loop win positions are in the currently selected choices of player 1
+            //_.difference takes 2 arrays and return an array of values that are unique to the first list
+            //if there are no unique values the players choices satisy a win condition.
             if(_.difference(testWin, this.xChoices).length === 0){
                 for(j = 0; j < testWin.length; j++){
-                    this.winSquares.push(testWin[j]);
-                    this.winner = this.player1;
+                    this.winSquares.push(testWin[j]); // populates an array with the winning matrix positions
+                    this.winner = this.player1; // sets the winner
                 };
+            //check if the current loop win positions are in the currently selected choices of player 2
+            //same logic as above test condition for player 2.
             }else if(_.difference(testWin, this.oChoices).length === 0){
                 for(j = 0; j < testWin.length;j++){
-                    this.winSquares.push(testWin[j]);
-                    this.winner = this.player2;
+                    this.winSquares.push(testWin[j]); // populates an array with the winning matrix positions
+                    this.winner = this.player2; // sets the winner
                 };
             }
         }
+        // return true if a winner has been found
         if(this.winner !== ''){
             return true;
+        // if no winner and no choices left the game is is tie
         }else if(this.winner === '' && this.turnCounter === 9){
             this.winner = 'Tie';
+            //collect all square to show a tie animation
             this.winSquares = ['00','01','02','10','11','12','20','21','22'];
             return true;
         }
     },
-    winSquares: [],
-    winner: '',
+    //produces a message for the screen to display with the winner
     winnerText: function(){
         let winText = ''
         if(this.player1 === this.winner){
@@ -72,12 +79,15 @@ const gameInfo = {
             this.score2 += 1;
         }
     },
+    //clears the board for the next round
     nextRound: function (){
         this.xChoices = [],
         this.oChoices = [],
         this.turnCounter = 0;
         this.winSquares = [];
+        this.winner = "";
     },
+    //clear board and current players to allow new players
     resetGame: function (){
         this.nextRound();
         this.player1 = 'Player 1';
